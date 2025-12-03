dedd032dc8417ece42fca2d816d12621112a1957
keithhegit <keith@ogcloud.com>
2025-12-03 16:35:03 +0800

    Fix club logo mappings to match actual database order

--- data/clubLogos.ts
+++ data/clubLogos.ts
@@ -3,49 +3,49 @@
 
 // ID-based mapping (RECOMMENDED - Primary mapping method)
 export const CLUB_LOGO_IDS_BY_ID: Record<number, string> = {
-    // English Premier League (league_id = 1)
-    1: '679',      // 曼城 (Man City)
-    2: '602',      // 阿森纳 (Arsenal)
-    3: '603',      // 阿斯顿维拉 (Aston Villa)
-    4: '680',      // 曼联 (Man United)
-    5: '630',      // 切尔西 (Chelsea)
-    6: '676',      // 利物浦 (Liverpool)
-    7: '728',      // 热刺 (Tottenham)
-    8: '688',      // 纽卡斯尔 (Newcastle)
-    9: '614',      // 布莱顿 (Brighton)
-    10: '736',     // 西汉姆 (West Ham)
-    11: '650',     // 埃弗顿 (Everton)
-    12: '699',     // 诺丁汉森林 (Nottm Forest)
-    13: '673',     // 莱斯特城 (Leicester)
-    14: '671',     // 利兹联 (Leeds)
-    15: '609',     // 伯恩茅斯 (Bournemouth)
-    16: '740',     // 狼队 (Wolves)
-    17: '654',     // 富勒姆 (Fulham)
-    18: '719',     // 南安普顿 (Southampton)
-    19: '642',     // 水晶宫 (Crystal Palace)
-    20: '127958',  // 布伦特福德 (Brentford)
+    // English Premier League (league_id = 1) - Correct order from DB
+    1: '679',      // Man City
+    2: '676',      // Liverpool
+    3: '728',      // Tottenham
+    4: '680',      // Man UFC (Man United)
+    5: '630',      // Chelsea
+    6: '602',      // Arsenal
+    7: '736',      // West Ham
+    8: '603',      // Aston Villa
+    9: '650',      // Everton
+    10: '609',     // Bournemouth
+    11: '688',     // Newcastle
+    12: '673',     // Leicester
+    13: '740',     // Wolves
+    14: '642',     // Crystal Palace
+    15: '671',     // Leeds
+    16: '654',     // Fulham
+    17: '719',     // Southampton
+    18: '699',     // Nottm Forest
+    19: '614',     // Brighton
+    20: '127958',  // Brentford
 
-    // Spanish La Liga (league_id = 2)
-    21: '748',     // 巴塞罗那 (Barcelona)
-    22: '750',     // 皇家马德里 (R. Madrid)
-    23: '751',     // 马德里竞技 (A. Madrid)
-    24: '794',     // 塞维利亚 (Sevilla)
-    25: '828',     // 皇家社会 (Real San Sebastián)
-    26: '797',     // 瓦伦西亚 (Valencia)
-    27: '806',     // 加的斯 (Cádiz)
-    28: '795',     // 皇家贝蒂斯 (Real Hispalis)
-    29: '752',     // 毕尔巴鄂竞技 (A. Bilbao)
-    30: '773',     // 赫塔菲 (Getafe)
-    31: '798',     // 比利亚雷亚尔 (Villarreal)
-    32: '765',     // 西班牙人 (Espanyol)
-    33: '790',     // 巴列卡诺 (Vallecano)
-    34: '799',     // 维戈塞尔塔 (Vigo)
-    35: '781',     // 马洛卡 (Mallorca)
-    36: '796',     // 巴拉多利德 (Valladolid)
-    37: '772',     // 赫罗纳 (Girona)
-    38: '753',     // 奥萨苏纳 (Atlético Pamplona)
-    39: '746',     // 阿尔梅里亚 (Almería)
-    40: '764',     // 埃尔切 (Elche)
+    // Spanish La Liga (league_id = 2) - Need to verify order
+    21: '748',     // Barcelona
+    22: '750',     // R. Madrid
+    23: '751',     // A. Madrid
+    24: '794',     // Sevilla
+    25: '828',     // Real San Sebastián
+    26: '797',     // Valencia
+    27: '806',     // Cádiz
+    28: '795',     // Real Hispalis (Real Betis)
+    29: '752',     // A. Bilbao
+    30: '773',     // Getafe
+    31: '798',     // Villarreal
+    32: '765',     // Espanyol
+    33: '790',     // Vallecano
+    34: '799',     // Vigo
+    35: '781',     // Mallorca
+    36: '796',     // Valladolid
+    37: '772',     // Girona
+    38: '753',     // Atlético Pamplona (Osasuna)
+    39: '746',     // Almería
+    40: '764',     // Elche
 };
 
 // Name-based mapping (LEGACY - Keep for backwards compatibility)
